203aa72510b57d44461f4da02e09895bd8219ae8
Andrey Bannikov <andrey137137137>
2019-04-21 23:59:28 +0300

    Refactored gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,97 +1,148 @@
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    // cssGlobbing = require('gulp-css-globbing'),
-    concatCss = require('gulp-concat-css'),
-    concat = require('gulp-concat'),
-    autoprefixer = require('gulp-autoprefixer'),
-    rename = require("gulp-rename"),
-    minifyCSS = require('gulp-minify-css'),
-    notify = require("gulp-notify"),
-    pug = require('gulp-pug'),
-    googleCdn = require('gulp-google-cdn'),
-    gulpFilter = require('gulp-filter'),
-    mainBowerFiles = require('gulp-main-bower-files');
+const // general
+  gulp = require("gulp"),
+  // плагины галпа, объявлять не нужно, используем как $gp.имяПлагина (без приставки gulp-)
+  $gp = require("gulp-load-plugins")(),
+  del = require("del"),
+  cssnext = require("postcss-cssnext"),
+  short = require("postcss-short"),
+  shortText = require("postcss-short-text"),
+  shortBorder = require("postcss-short-border"),
+  webpack = require("webpack"),
+  webpackConfig = require("./webpack.config.js"),
+  browserSync = require("browser-sync").create(),
+  pathes = {
+    src: "src",
+    dest: "app",
+    html: {
+      src: "/pug",
+      dest: ""
+    },
+    fonts: {
+      src: "/fonts",
+      dest: "/fonts"
+    },
+    images: {
+      src: "/images",
+      dest: "/img"
+    },
+    css: {
+      src: "/scss",
+      dest: "/css"
+    },
+    js: {
+      src: "/js",
+      dest: "/js"
+    }
+  };
 
-// gulp.task('connect', function () {
-//   connect.server({
-//     root: "app",
-//     // port: 9000,
-//     // livereload: true,
-//     middleware: function (connect, options) {
+for (path in pathes) {
+  if (!pathes[path].src) continue;
 
-//         options.rule = [/\.do/, /\.jsp/, /\.htm/];
-// //or        options.rule = /\.do/; 
+  pathes[path].src = pathes.src + pathes[path].src;
+  pathes[path].dest = pathes.dest + pathes[path].dest;
+}
 
-//         options.server = "127.0.0.1:8081";
+function clean() {
+  return del([
+    pathes.dest + "/*",
+    // pathes.dest + "/*.html",
+    "!" + pathes.fonts.dest,
+    "!" + pathes.images.dest,
+    "!" + pathes.dest + "/public"
+  ]);
+}
 
-//         var proxy = new Reproxy(options);
-
-//         return [proxy];
-//     }
-//   });
-// });
-
-gulp.task('html', function () {
+function html() {
   var YOUR_LOCALS = {};
 
-  return gulp.src('pug/index.pug')
-  // return gulp.src('index.html')
-    .pipe(pug({
-      locals: YOUR_LOCALS,
-      pretty: true
-    }))
-    .pipe(googleCdn(require('./bower.json')))
-    .pipe(gulp.dest('app'));
-    // .pipe(connect.reload());
-});
+  return gulp
+    .src(pathes.html.src + "/*.pug")
+    .pipe($gp.plumber())
+    .pipe(
+      $gp.pug({
+        locals: YOUR_LOCALS,
+        pretty: true
+      })
+    )
+    .pipe(gulp.dest(pathes.html.dest));
+}
 
-gulp.task('css_libs', function() {
-  var filterJS = gulpFilter('**/*.css', {restore: true});
+function css() {
+  var plugins = [
+    // precss(),
+    cssnext(),
+    // colorAlpha(),
+    short(),
+    // shortFont(),
+    shortText(),
+    shortBorder()
+    // assets({
+    //   loadPaths: [pathes.images.dest]
+    // }),
+    // minmax(),
+    // autoprefixer({browsers: ['last 2 version']}),
+    // cssnano()
+  ];
 
-  return gulp.src('./bower.json')
-    .pipe(mainBowerFiles())
-    .pipe(filterJS)
-    .pipe(gulp.dest('app/css/libs'));
-});
+  return (
+    gulp
+      .src(pathes.css.src + "/style.scss")
+      .pipe($gp.plumber())
+      // .pipe(cssGlobbing())
+      .pipe($gp.sass().on("error", $gp.sass.logError))
+      .pipe($gp.postcss(plugins))
+      // .pipe(autoprefixer({
+      //   browsers: ['last 15 versions'],
+      //   cascade: false
+      // }))
+      .pipe($gp.concatCss("bundle.css"))
+      // .pipe(minifyCSS())
+      .pipe($gp.rename("style.min.css"))
+      .pipe(gulp.dest(pathes.css.dest))
+  );
+}
 
-gulp.task('css', function () {
-  return gulp.src('scss/*.scss')
-    // .pipe(cssGlobbing())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(concatCss("bundle.css"))
-    .pipe(autoprefixer({
-        browsers: ['last 15 versions'],
-        cascade: false
-    }))
-    // .pipe(minifyCSS())
-    .pipe(rename("style.min.css"))
-    .pipe(gulp.dest('app/css'));
-    // .pipe(connect.reload());
-});
+function js() {
+  return (
+    gulp
+      .src(pathes.js.src + "/*.js")
+      // .pipe($gp.sourcemaps.init())
+      .pipe($gp.webpack(webpackConfig, webpack))
+      // .pipe(concat('script.min.js'))
+      .pipe($gp.sourcemaps.write())
+      .pipe(gulp.dest(pathes.js.dest))
+  );
+}
 
-gulp.task('js_libs', function() {
-  var filterJS = gulpFilter('**/*.js', {restore: true});
+function browser_sync() {
+  browserSync.init({
+    server: pathes.dest
+    // notify: false
+  });
+  browserSync.watch(pathes.dest + "/**/*.*", browserSync.reload);
+}
 
-  return gulp.src('./bower.json')
-    .pipe(mainBowerFiles())
-    .pipe(filterJS)
-    .pipe(gulp.dest('app/js/libs'));
-});
+function watch() {
+  gulp.watch(`${pathes.html.src}/*.pug`, gulp.series(html));
+  gulp.watch(
+    [pathes.css.src + "/*.scss", pathes.css.src + "/normalize/*.scss"],
+    gulp.series(css)
+  );
+  gulp.watch(pathes.js.src + "/script.js", gulp.series(js));
+}
 
-gulp.task('js', function () {
+exports.clean = clean;
+exports.html = html;
+exports.css = css;
+exports.js = js;
+exports.watch = watch;
+exports.browser_sync = browser_sync;
 
-  return gulp.src('js/*.js')
-    .pipe(concat('script.min.js'))
-    .pipe(gulp.dest('app/js'));
-    // .pipe(connect.reload());
-});
-
-gulp.task('watch', function () {
-  gulp.watch('pug/*.pug', ['html']);
-  gulp.watch(['scss/*.scss', 'scss/normalize/*.scss'], ['css']);
-  gulp.watch('js/*.js', ['js']);
-});
-
-// gulp.task('default', ['connect', 'html', 'css', 'js', 'watch']);
-gulp.task('default', ['html', 'css_libs', 'css', 'js_libs', 'js', 'watch']);
-// gulp.task('default', ['html', 'css_libs', 'css', 'js_libs', 'js']);
+gulp.task(
+  "default",
+  gulp.series(
+    clean,
+    gulp.parallel(html, css, js),
+    gulp.parallel(watch, browser_sync)
+  )
+);
